5ac5c83e9f099f63d9faf07e6800407111a5dc34
Stacey Astoveza <staceyastoveza@transfix.io>
2017-08-04 14:13:29 -0400

    done

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
 
+def sort_array_asc(arr)
+  arr.sort
+end
+
+def sort_array_desc(arr)
+  arr.sort {|x, y| y <=> x}
+end
+
+def sort_array_char_count(arr)
+  arr.sort_by {|element| element.length }
+end
+
+def swap_elements(arr)
+  last_name = arr.pop
+  arr.insert(1, last_name)
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  arr.map do |word|
+    word[0..1] + '$' + word[3..(word.length)]
+  end
+end
+
+def find_a(arr)
+  arr.select { |element| element.start_with?('a') }
+end
+
+def sum_array(arr)
+  arr.reduce(:+)
+end
+
+def add_s(arr)
+  arr.map do |word|
+     unless arr[1] == word
+       word += 's'
+     else
+       word
+     end
+   end
+end
